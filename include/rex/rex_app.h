111f3b25589e52615c4fc181386d945f9d61af8b
Marco Andronaco <andronacomarco@gmail.com>
2026-08-03 23:50:45 +0200

    feat(ui): add hint toast at game start

--- include/rex/rex_app.h
+++ include/rex/rex_app.h
@@ -56,6 +56,7 @@ struct PathConfig {
 namespace ui {
 class AchievementNotificationDialog;
 class ConsoleDialog;
+class HintToastDialog;
 class SettingsDialog;
 class ShaderDebuggerDialog;
 }  // namespace ui
@@ -355,6 +356,9 @@ class ReXApp : public ui::WindowedApp, public ui::WindowListener, public ui::Win
   std::unique_ptr<ui::ImGuiDialog> achievements_overlay_;
   std::shared_ptr<ui::AchievementNotificationDialog> achievement_notification_;
   uint64_t achievement_notification_listener_ = 0;
+  // Shows RuntimeConfig::startup_hint once, as the game starts (see
+  // LaunchModule).
+  std::unique_ptr<ui::HintToastDialog> hint_toast_;
   std::unique_ptr<ui::ShaderDebuggerDialog> shader_debugger_overlay_;
   ui::DebugOverlayDialog::FrameStatsProvider frame_stats_provider_;
   ShaderDebuggerOverride shader_debugger_override_;
